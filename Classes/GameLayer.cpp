8d815a35f435ce4cb3a4d47a68a88bc274849848
unlimitloli <820478764@qq.com>
2017-02-14 01:18:43 +0800

    创建卡牌对象数据和精灵

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -2,6 +2,7 @@
 #include "GameMaster.h"
 #include "ui\CocosGUI.h"
 #include "cocostudio\CocoStudio.h"
+#include "CardSprite.h"
 
 USING_NS_CC;
 using namespace cocos2d::ui;
@@ -24,5 +25,10 @@ bool GameLayer::init()
 void GameLayer::setReady()
 {
 	auto Panel_Game = getWidgetByName(m_root, "Panel_Game");
-	Panel_Game->setVisible(false);
+	//Panel_Game->setVisible(false);
+	auto Panel_Self = getWidgetByName(m_root, "Panel_Self");
+	auto sprite = CardSprite::create(16, 1);
+	sprite->setPosition(100, 100);
+	Panel_Self->removeAllChildrenWithCleanup(true);
+	Panel_Self->addChild(sprite);
 }
